939c7aca3f9b0b17dff553c1a3e52bd943e71097
yangyixin <yix2730555@yahoo.com>
2017-04-12 19:20:49 +0800

    show group's creator info

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  hsa_many :groups
+  has_many :groups
 end
